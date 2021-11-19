79b3c396ad3c957d57f6ae8d4997bade7bb4484a
SokolArr <alex970899@gmail.com>
2021-11-19 14:16:05 +0300

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Form />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Form from "./components/Form.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Form
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: "Roboto", sans-serif;
+  color: #2a2a2a;
+  padding: 24px;
+  box-sizing: border-box;
+}
+
+html,
+body {
+  font-size: 18px;
+}
+
+#app {
+  height: 100%;
+  align-items: center;
+}
+
+* {
+  box-sizing: border-box;
 }
 </style>
